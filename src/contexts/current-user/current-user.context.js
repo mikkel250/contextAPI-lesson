b56d4557c5e6a731f4a132b448a7f5c9f5c6df18
Mikkel Ridley <mikkel250@gmail.com>
2019-10-03 15:38:41 -0700

    add context API to header, collection

---
+++ src/contexts/current-user/current-user.context.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const CurrentUserContext = createContext(undefined); // set the initial value of the current user as undefined
+
+export default CurrentUserContext;
